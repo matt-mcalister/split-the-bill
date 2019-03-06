38233df5db826e963d8075e4639ebc8db985b88c
matt-mcalister <matt.mcalister93@gmail.com>
2019-03-06 09:59:46 -0500

    created basic structure of transaction view

---
+++ containers/TransactionView.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {
+  View
+} from "react-native"
+
+class TransactionView extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      peopleIds: []
+    }
+  }
+
+  render(){
+    return (
+      <View>
+        // edit price and description
+        // see shot of lineitem
+        // pick person
+        // confirm buttons
+      </View>
+    )
+  }
+}
